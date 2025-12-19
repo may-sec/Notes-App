1892a20d4bc153f4f79f304cd01c0894601ae8a1
Mayank <mayankrawatp001@gmail.com>
2025-12-19 17:49:36 +0530

    updated button

--- src/App.jsx
+++ src/App.jsx
@@ -43,21 +43,19 @@ const App = () => {
 
         <h1 className='text-4xl mb-2 font-bold'>Add Notes</h1>
 
-        {/* PEHLA INPUT FOR HEADING */}
         <input
           type="text"
           placeholder='Enter Notes Heading'
-          className='px-5 w-full font-medium py-2 border-2 outline-none rounded '
+          className='px-5 w-full font-medium py-2 border-2 outline-none rounded hover:bg-gray-900'
           value={title}
           onChange={(e) => {
             setTitle(e.target.value)
           }}
         />
 
-        {/* DETAILED VALA INPUT  */}
         <textarea
           type="text"
-          className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded '
+          className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded hover:bg-gray-900'
           placeholder='Write Details here'
           value={details}
           onChange={(e) => {
@@ -66,7 +64,7 @@ const App = () => {
         />
 
         <button
-          className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded'
+          className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded hover:bg-gray-300'
         >
           Add Note
         </button>
@@ -80,11 +78,11 @@ const App = () => {
             return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
               <div>
                 <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
-                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.details}</p>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600 w-40 '>{elem.details}</p>
               </div>
               <button onClick={() => {
                 deleteNote(idx)
-              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
+              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white hover:bg-red-300'>Delete</button>
             </div>
           })}
         </div>
